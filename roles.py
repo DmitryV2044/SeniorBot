23548d021223c782ef4ccb8386a677c9f610e780
Dmitriy V <75983297+DmitryV2044@users.noreply.github.com>
2022-10-12 17:51:19 +0500

    Добавлены новые роли

--- roles.py
+++ roles.py
@@ -1,2 +1,2 @@
-custom_roles = {'Python', 'C#', 'C++', 'Blender', 'Unreal Engine', 'Unity', 'Godot'}
+custom_roles = {'Python', 'C#', 'C++', 'Blender', 'Unreal Engine', 'Unity', 'Godot', 'Java Script', 'PHP', 'Java', 'C'}
 
